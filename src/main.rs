a52de74e4bb19514a47016ecd4e51871ebbb19af
AmossGuy <yellowilya@gmail.com>
2020-05-20 11:52:58 -0400

    add input code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use cursive::Cursive;
 use cursive::Printer;
 use cursive::XY;
-use cursive::event::{Event, EventResult};
+use cursive::event::{Event, EventResult, Key};
 use cursive::vec::Vec2;
 use cursive::views::Dialog;
 
@@ -21,6 +21,11 @@ impl SokobanTile {
     }
 }
 
+struct SokobanTilemap {
+    width: usize,
+    tiles: Vec<SokobanTile>,
+}
+
 #[derive(PartialEq)]
 enum SokobanObjectType {
     Player,
@@ -33,13 +38,12 @@ struct SokobanObject {
 }
 
 impl SokobanObject {
-    // fn r#move(&mut self, delta: XY<isize>, game: &SokobanGame) {
-    // }
+    fn r#move(&mut self, delta: XY<isize>, tilemap: &SokobanTilemap) {
+    }
 }
 
 struct SokobanGame {
-    width: usize,
-    tilemap: Vec<SokobanTile>,
+    tilemap: SokobanTilemap,
     objects: Vec<SokobanObject>,
 }
 
@@ -48,13 +52,15 @@ impl SokobanGame {
         assert!(levelstring.chars().count() % width == 0);
 
         let mut game = SokobanGame {
-            width,
-            tilemap: vec![SokobanTile::Empty; levelstring.chars().count()],
+            tilemap: SokobanTilemap {
+                width,
+                tiles: vec![SokobanTile::Empty; levelstring.chars().count()],
+            },
             objects: Vec::new(),
         };
 
         for (i, c) in levelstring.chars().enumerate() {
-            game.tilemap[i] = match c {
+            game.tilemap.tiles[i] = match c {
                 ' ' | '$' | '@' => SokobanTile::Empty,
                 '.' | '*' | '+' => SokobanTile::Goal,
                 '#' => SokobanTile::Wall,
@@ -64,11 +70,11 @@ impl SokobanGame {
             match c {
                 '@' | '+' => game.objects.push(SokobanObject {
                     r#type: SokobanObjectType::Player,
-                    pos: Vec2::new(i % game.width, i / game.width),
+                    pos: Vec2::new(i % game.tilemap.width, i / game.tilemap.width),
                 }),
                 '$' | '*' => game.objects.push(SokobanObject {
                     r#type: SokobanObjectType::Box,
-                    pos: Vec2::new(i % game.width, i / game.width),
+                    pos: Vec2::new(i % game.tilemap.width, i / game.tilemap.width),
                 }),
                 _ => (),
             };
@@ -92,8 +98,8 @@ impl SokobanView {
 
 impl cursive::view::View for SokobanView {
     fn draw(&self, printer: &Printer) {
-        for (i, t) in self.game.tilemap.iter().enumerate() {
-            printer.print(Vec2::new(i % self.game.width, i / self.game.width), match t {
+        for (i, t) in self.game.tilemap.tiles.iter().enumerate() {
+            printer.print(Vec2::new(i % self.game.tilemap.width, i / self.game.tilemap.width), match t {
                 SokobanTile::Empty => " ",
                 SokobanTile::Goal => ".",
                 SokobanTile::Wall => "#",
@@ -101,7 +107,7 @@ impl cursive::view::View for SokobanView {
         }
 
         for o in self.game.objects.iter() {
-            printer.print(o.pos, match self.game.tilemap[o.pos.x + o.pos.y * self.game.width] {
+            printer.print(o.pos, match self.game.tilemap.tiles[o.pos.x + o.pos.y * self.game.tilemap.width] {
                 SokobanTile::Empty => match o.r#type {
                     SokobanObjectType::Player => "@",
                     SokobanObjectType::Box => "$",
@@ -116,10 +122,27 @@ impl cursive::view::View for SokobanView {
     }
 
     fn required_size(&mut self, _constraint: Vec2) -> Vec2 {
-        return Vec2::new(self.game.width, self.game.tilemap.len() / self.game.width);
+        return Vec2::new(self.game.tilemap.width, self.game.tilemap.tiles.len() / self.game.tilemap.width);
     }
     fn on_event(&mut self, event: Event) -> EventResult {
         match event {
+            Event::Key(key) => {
+                let delta = match key {
+                    Key::Left => XY::<isize>::new(-1, 0),
+                    Key::Right => XY::<isize>::new(1, 0),
+                    Key::Up => XY::<isize>::new(0, -1),
+                    Key::Down => XY::<isize>::new(0, 1),
+                    _ => return EventResult::Ignored,
+                };
+
+                for object in &mut self.game.objects {
+                    if object.r#type == SokobanObjectType::Player {
+                        object.r#move(delta, &self.game.tilemap);
+                    }
+                }
+
+                EventResult::Consumed(None)
+            }
             _ => EventResult::Ignored,
         }
     }
