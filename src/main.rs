7dc0e44ffa7c5158b54c29aafd8a40313af711de
AmossGuy <yellowilya@gmail.com>
2020-08-06 18:32:32 -0400

    add title screen

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use cursive::{Cursive, CursiveExt};
 use cursive::event::Key;
 use cursive::view::Nameable;
-use cursive::views::Dialog;
+use cursive::views::{Dialog, TextView};
 
 mod gamemodel;
 mod gameview;
@@ -13,7 +13,31 @@ fn main() {
     siv.add_global_callback('q', |s| s.quit());
     siv.add_global_callback(Key::Esc, |s| s.quit());
 
-    siv.add_layer(Dialog::around(
+    show_intro(&mut siv);
+
+    siv.run();
+}
+
+fn show_intro(s: &mut Cursive) {
+    let message = "\
+        Sokoban in Rust by AmossGuy\n\
+        \n\
+        Arrow keys or WASD to move\n\
+        R to restart level, U to undo\n\
+        Q to quit\n\
+    ";
+
+    let dialog = Dialog::new()
+        .title("Sokoban")
+        .content(TextView::new(message))
+        .button("Play", |s| {s.pop_layer(); show_game(s)})
+        .button("Quit", |s| s.quit());
+
+    s.add_layer(dialog);
+}
+
+fn show_game(s: &mut Cursive) {
+    s.add_layer(Dialog::around(
         GameView::new(1, |s| {
             let mut dialog = Dialog::text("You win!");
             if s.call_on_name("gameview", |view: &mut GameView| {
@@ -31,6 +55,4 @@ fn main() {
             s.add_layer(dialog);
         }).with_name("gameview")
     ).title("Sokoban"));
-
-    siv.run();
 }
